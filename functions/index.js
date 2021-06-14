903bfa4ad0d6c1470bdd59d41089bfc1ca4c040e
Gonzalo Fleming <gonzafg2@gmail.com>
2021-06-13 21:16:17 -0400

    Varias apis para mis alumnos y yo

--- functions/index.js
+++ functions/index.js
@@ -45,3 +45,63 @@ exports.planes_moviles = functions.https.onRequest((req, res) => {
     });
   }
 });
+
+exports.planes_crossfit = functions.https.onRequest((req, res) => {
+  res.header("Content-Type", "application/json");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  if (req.method === "GET") {
+    const data = firebase.database().ref("/planes_crossfit"); // Hacemos referencia a la base de datos
+    data.on("value", (snapshot) => {
+      res.json(snapshot.val()); // El elemento resultante lo exponemos en un archivo JSON
+    });
+  }
+});
+
+exports.visumtech = functions.https.onRequest((req, res) => {
+  res.header("Content-Type", "application/json");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  if (req.method === "GET") {
+    const data = firebase.database().ref("/visumtech"); // Hacemos referencia a la base de datos
+    data.on("value", (snapshot) => {
+      res.json(snapshot.val()); // El elemento resultante lo exponemos en un archivo JSON
+    });
+  }
+});
+
+exports.regiones_comunas = functions.https.onRequest((req, res) => {
+  res.header("Content-Type", "application/json");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  if (req.method === "GET") {
+    const data = firebase.database().ref("/regiones_comunas"); // Hacemos referencia a la base de datos
+    data.on("value", (snapshot) => {
+      res.json(snapshot.val()); // El elemento resultante lo exponemos en un archivo JSON
+    });
+  }
+});
+
+exports.bees = functions.https.onRequest((req, res) => {
+  res.header("Content-Type", "application/json");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  if (req.method === "GET") {
+    const data = firebase.database().ref("/bees"); // Hacemos referencia a la base de datos
+    data.on("value", (snapshot) => {
+      res.json(snapshot.val()); // El elemento resultante lo exponemos en un archivo JSON
+    });
+  }
+});
+
+exports.sucursales_scotia = functions.https.onRequest((req, res) => {
+  res.header("Content-Type", "application/json");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  if (req.method === "GET") {
+    const data = firebase.database().ref("/sucursales_scotia"); // Hacemos referencia a la base de datos
+    data.on("value", (snapshot) => {
+      res.json(snapshot.val()); // El elemento resultante lo exponemos en un archivo JSON
+    });
+  }
+});
